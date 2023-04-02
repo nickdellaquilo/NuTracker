73f63730167e1ad914ef12baf49ccb27e167982c
Nick Dell'Aquilo <nickdellaquilo@protonmail.com>
2023-04-02 18:47:52 -0400

    refactoring

---
+++ DRI/DRI.py
@@ -0,0 +1,25 @@
+def get_values():
+    #age
+    #sex
+    #weight
+    #unit (imperial/metric)
+    return;
+
+
+def caloriesDRI():
+
+    return;
+
+def carbsDRI():
+
+    return;
+
+def fatDRI():
+    return;
+
+def vitDRI():
+    return;
+
+def minDRI():
+    return;
+

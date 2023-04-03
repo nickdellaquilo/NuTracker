3a3907e87fef2499b1b5f8822b9bc65121c1d0a1
Nick Dell'Aquilo <nickdellaquilo@protonmail.com>
2023-04-03 01:22:55 -0400

    dri calc

---
+++ domain/src/main/python/dri_calculator.py
@@ -0,0 +1,17 @@
+def dri_calculator(age, weight, height, gender, activity_level):
+    if gender == "male":
+        bmr = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
+    else:
+        bmr = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age)
+
+    activity_multipliers = {
+        "sedentary": 1.2,
+        "lightly_active": 1.375,
+        "moderately_active": 1.55,
+        "very_active": 1.725,
+        "extra_active": 1.9
+    }
+
+    tdee = bmr * activity_multipliers[activity_level]
+
+    return tdee

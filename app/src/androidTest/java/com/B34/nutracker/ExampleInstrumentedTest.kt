c37be093d68b3e33a5d4f12793b861dc1236268a
tk2558 <tk2558@nyu.edu>
2023-04-19 13:23:08 -0400

    no message

--- app/src/androidTest/java/com/B34/nutracker/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/B34/nutracker/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.nutrackeralpha
+package com.B34.nutracker
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.nutrackeralpha", appContext.packageName)
+        assertEquals("com.B34.nutracker", appContext.packageName)
     }
 }
---
+++ app/src/main/java/com/B34/nutracker/search_Fragment.kt
@@ -0,0 +1,34 @@
+package com.B34.nutracker
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [search_Fragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class search_Fragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_search_, container, false)
+
+        return view
+    }
+}
